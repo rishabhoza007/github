9e2f3471c344147897919548656b901f7d54cec4
Ram <Ram@DESKTOP-OO8CKP7>
2021-08-03 12:45:59 +0530

    [mandakini]:repositories

---
+++ src/github_vcs/demo1.java
@@ -0,0 +1,20 @@
+package github_vcs;
+
+public class demo1 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		
+		System.out.println("github repository");
+		System.out.println("github repository");
+		System.out.println("github repository");
+		System.out.println("github repository");
+		System.out.println("github repository");
+
+		
+	}
+	
+	
+}
